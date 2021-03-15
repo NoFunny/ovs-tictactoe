ea4de2adc429288c479cac5fd9630fcf2df742c9
NoFunny <chunikhin_1999@mail.ru>
2021-03-15 12:01:43 +0700

    Add skeleton of the project

---
+++ TicTacToe/src/server/server.java
@@ -0,0 +1,199 @@
+package server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.concurrent.Executors;
+
+public class server {
+
+    private static File f;
+    private static FileChannel channel;
+    private static FileLock lock;
+    
+    public static void main(String[] args) throws Exception {
+            //Your application tasks here..
+            connectionNewServer();
+            try (var listener = new ServerSocket(Integer.parseInt(args[0]))) {
+                System.out.println("Tic Tac Toe Server is Running on address: 127.0.0.1, port: "+listener.getLocalPort()+"...");
+                var pool = Executors.newFixedThreadPool(200);
+                while (true) {
+                    if (!f.exists()) {
+                        connectionNewServer();
+                    }
+                    Game game = new Game();
+                    pool.execute(game.new Player(listener.accept(), 'X'));
+                    pool.execute(game.new Player(listener.accept(), 'O'));
+                }
+            }
+
+    }
+    public static void connectionNewServer() {
+        try {
+            f = new File("123.lock");
+            // Check if the lock exist
+//            if (f.exists()) {
+//                // if exist try to delete it
+////                f.delete();
+//                System.out.println("123");
+//            }
+            // Try to get the lock
+            channel = new RandomAccessFile(f, "rw").getChannel();
+            lock = channel.tryLock();
+            if (lock == null) {
+                // File is lock by other application
+                channel.close();
+                System.out.println("It's children server...");
+            } else {
+                System.out.println("It's header server...");
+            }
+            // Add shutdown hook to release lock when application shutdown
+            ShutdownHook shutdownHook = new ShutdownHook();
+            Runtime.getRuntime().addShutdownHook(shutdownHook);
+        }catch(IOException e) {
+                throw new RuntimeException("Could not start process.", e);
+        }
+    }
+    public static void unlockFile() {
+        // release and delete file lock
+        try {
+            if(lock != null) {
+                System.out.println("Lock file has been unlock");
+                lock.release();
+                channel.close();
+                f.delete();
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static class ShutdownHook extends Thread {
+
+        public void run() {
+            unlockFile();
+        }
+    }
+}
+
+class Game {
+
+    // Board cells numbered 0-8, top to bottom, left to right; null if empty
+    private Player[] board = new Player[9];
+
+    Player currentPlayer;
+
+    public boolean hasWinner() {
+        return (board[0] != null && board[0] == board[1] && board[0] == board[2])
+                || (board[3] != null && board[3] == board[4] && board[3] == board[5])
+                || (board[6] != null && board[6] == board[7] && board[6] == board[8])
+                || (board[0] != null && board[0] == board[3] && board[0] == board[6])
+                || (board[1] != null && board[1] == board[4] && board[1] == board[7])
+                || (board[2] != null && board[2] == board[5] && board[2] == board[8])
+                || (board[0] != null && board[0] == board[4] && board[0] == board[8])
+                || (board[2] != null && board[2] == board[4] && board[2] == board[6]);
+    }
+
+    public boolean boardFilledUp() {
+        return Arrays.stream(board).allMatch(Objects::nonNull);
+    }
+
+    public synchronized void move(int location, Player player) {
+        if (player != currentPlayer) {
+            throw new IllegalStateException("Not your turn");
+        } else if (player.opponent == null) {
+            throw new IllegalStateException("You don't have an opponent yet");
+        } else if (board[location] != null) {
+            throw new IllegalStateException("Cell already occupied");
+        }
+        board[location] = currentPlayer;
+        currentPlayer = currentPlayer.opponent;
+    }
+
+    /**
+     * A Player is identified by a character mark which is either 'X' or 'O'. For
+     * communication with the client the player has a socket and associated Scanner
+     * and PrintWriter.
+     */
+    class Player implements Runnable {
+        char mark;
+        Player opponent;
+        Socket socket;
+        Scanner input;
+        PrintWriter output;
+
+        public Player(Socket socket, char mark) {
+            this.socket = socket;
+            this.mark = mark;
+        }
+
+        @Override
+        public void run() {
+            try {
+                setup();
+                processCommands();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (opponent != null && opponent.output != null) {
+                    opponent.output.println("OTHER_PLAYER_LEFT");
+                }
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+
+        private void setup() throws IOException {
+            input = new Scanner(socket.getInputStream());
+            output = new PrintWriter(socket.getOutputStream(), true);
+            output.println("WELCOME " + mark);
+            if (mark == 'X') {
+                currentPlayer = this;
+                output.println("MESSAGE Waiting for opponent to connect");
+            } else {
+                opponent = currentPlayer;
+                opponent.opponent = this;
+                opponent.output.println("MESSAGE Your move");
+            }
+        }
+
+        private void processCommands() {
+            while (input.hasNextLine()) {
+                var command = input.nextLine();
+                if (command.startsWith("QUIT")) {
+                    return;
+                } else if (command.startsWith("MOVE")) {
+                    processMoveCommand(Integer.parseInt(command.substring(5)));
+                }
+            }
+        }
+
+        private void processMoveCommand(int location) {
+            try {
+                move(location, this);
+                output.println("VALID_MOVE");
+                opponent.output.println("OPPONENT_MOVED " + location);
+                if (hasWinner()) {
+                    output.println("VICTORY");
+                    opponent.output.println("DEFEAT");
+                } else if (boardFilledUp()) {
+                    output.println("TIE");
+                    opponent.output.println("TIE");
+                }
+            } catch (IllegalStateException e) {
+                output.println("MESSAGE " + e.getMessage());
+            }
+        }
+    }
+}
